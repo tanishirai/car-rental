a8f1a58e0cbd6621145bec9bbc40d9608817e783
CyberMage7 <technician9012@gmail.com>
2025-01-03 20:46:57 +0530

    Updated About us Page

---
+++ client/src/components/Card/Content.js
@@ -0,0 +1,23 @@
+const content = [
+  {
+    id: 1,
+    title: "Real-time availability of cars",
+  },
+  {
+    id: 2,
+    title: "Flexible rental durations",
+  },
+  {
+    id: 3,
+    title: "Integrated payment gateway for convenience",
+  },
+  {
+    id: 4,
+    title: "Admin dashboards with insightful analytics",
+  },
+  {
+    id: 5,
+    title: "Comprehensive customer support",
+  },
+];
+export default content;
